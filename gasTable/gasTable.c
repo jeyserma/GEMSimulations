92157cf5ede549dfe4dc0474b418d3d27388e096
jeyserma <janeysermans@gmail.com>
2014-05-27 22:07:40 +0200

    Update gasTable.c

--- gasTable/gasTable.c
+++ gasTable/gasTable.c
@@ -32,47 +32,35 @@
 using namespace Garfield;
 const std::string GARFIELD = "/afs/cern.ch/user/j/jeyserma/garfieldpp/";
 
-TString int2str(Int_t t) {
-    TString str; 
-    str.Form("%d", t);
-    return str;
-} 
-
 int main(int argc, char * argv[]) {
        
-    TStopwatch watch;
+    	TStopwatch watch;
 
 	TApplication app("app", &argc, argv);
  	plottingEngine.SetDefaultStyle();
  	gRandom = new TRandom3(0); // set random seed
      
-    MediumMagboltz* gas = new MediumMagboltz();
-    gas->SetComposition("ar", 50., "co2", 25., "cf4", 25);
-    gas->SetTemperature(293.15);
-    gas->SetPressure(760.);
-  
-    gas->PrintGas();
-    // Calculation from 0 kV/cm to 40 kV/cm in 100 steps
-    gas->SetFieldGrid(0., 40000., 50, false, 0., 0., 1, 0., 0., 1);
-    
-    gas->EnableDebugging();
-    gas->Initialise();  
-    gas->DisableDebugging();
-    
-    // Disable penning transfer
-    //const double rPenning = 0.;
-    //const double lambdaPenning = 0.;
-    //gas->EnablePenningTransfer(rPenning, lambdaPenning, "ar");
-    gas->DisablePenningTransfer();
-    
-    // Generate gas table
-    gas->GenerateGasTable(5, true);
-    
-    gas->WriteGasFile("data/Ar-CO2-CF4-50-25-25.gas");
-    
-    std::cout << "--------------- TIMING ---------------" << std::endl;
-    std::cout << watch.CpuTime() << std::endl;
-
-    return 0;
-    app.Run(); 
+    	MediumMagboltz* gas = new MediumMagboltz();
+	    gas->SetComposition("Ar", 75., "CO2", 25.);
+	    gas->SetTemperature(293.15);
+	    gas->SetPressure(760.);
+	  
+	    gas->PrintGas();
+	    // Calculation from 0 kV/cm to 40 kV/cm in 50 steps
+	    gas->SetFieldGrid(0., 40000., 50, false, 0., 0., 1, 0., 0., 1);
+	    
+	    gas->EnableDebugging();
+	    gas->Initialise();  
+	    gas->DisableDebugging();
+	    
+	    // Generate gas table
+	    gas->GenerateGasTable(5, true);
+	    
+	    gas->WriteGasFile("data/Ar-CO2-75-25.gas");
+	    
+	    std::cout << "--------------- TIMING ---------------" << std::endl;
+	    std::cout << watch.CpuTime() << std::endl;
+	
+	    return 0;
+	    app.Run(); 
 }
